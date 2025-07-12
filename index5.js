31571fcdb4c3895c4464b395686c1c20ced9fcfb
padomDev <pdominguez@estudiante.ceibal.edu.uy>
2025-07-12 01:59:49 -0300

    ranking y animacion

--- index5.js
+++ index5.js
@@ -1,27 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // H1
-  const nuevoH1 = document.createElement('h1');
-  nuevoH1.textContent = 'FIN DEL JUEGO';
-  nuevoH1.classList.add('mover-desde-abajo');
-  document.getElementById('titulo').appendChild(nuevoH1);
+    // H1
+    const nuevoH1 = document.createElement('h1');
+    nuevoH1.textContent = 'FIN DEL JUEGO';
+    nuevoH1.classList.add('mover-desde-abajo');
+    document.getElementById('titulo').appendChild(nuevoH1);
 
-  // Forzar reflow del H1
-  void nuevoH1.offsetWidth;
+    // Forzar reflow del H1
+    void nuevoH1.offsetWidth;
 
-  // Activar transición del H1
-  nuevoH1.classList.add('mostrar');
+    // Activar transición del H1
+    nuevoH1.classList.add('mostrar');
 
-  // Sección
-  const section = document.querySelector('.section');
-  section.classList.add('aparece-desde-abajo');
+    // Sección
+    const section = document.querySelector('.section');
+    section.classList.add('aparece-desde-abajo');
 
-  // Forzar reflow de la sección
-  void section.offsetWidth;
+    // Forzar reflow de la sección
+    void section.offsetWidth;
 
-  setTimeout(()=>{
-      // Activar transición de la sección
-  section.classList.add('mostrar-resultado');
-  }, 1000);
+    setTimeout(() => {
+        // Activar transición de la sección
+        section.classList.add('mostrar-resultado');
+    }, 1000);
 
+    //Ranking
+    const ranking = document.querySelector('.ranking');
+    section.classList.add('aparece-desde-abajo');
+
+    // Forzar reflow del ranking
+    void section.offsetWidth;
+
+    setTimeout(() => {
+        // Activar transición de la sección
+     ranking.classList.add('mostrar-ranking');
+    }, 1500);
 });
 
